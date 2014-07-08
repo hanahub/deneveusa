5f480a323b9cedab5943510ec970b6173ce614d2
Valentin Marinov <dev.valentin2013@gmail.com>
2014-07-08 17:04:22 +0300

    test update

--- wp-content/themes/clipcart/home.php
+++ wp-content/themes/clipcart/home.php
@@ -102,7 +102,10 @@ genesis_before_content_sidebar_wrap(); ?>
             $loop_counter = 1;
             if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <li class="product <?php if ( $loop_counter == 4 ) { echo "product-last"; } ?>">
-                    <div class="product_wrap">   
+                    <div class="product_wrap">
+                    
+                    
+                    
                         <?php 
                         $title = get_the_title().' ';
                         $chars_title = strlen($title);
